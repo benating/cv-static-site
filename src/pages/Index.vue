a0e84e943329c400c92ac3d9f133fbeb01251fcb
Bernard Ting <ting.bsk97@gmail.com>
2020-09-02 12:25:41 +0100

    feat: add gridsome default starter project and initial pages

---
+++ src/pages/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <Layout>
+
+    <g-image alt="Photo of myself" src="~/profile.png" width="135" />
+
+    <h1>Bernard Ting</h1>
+
+    <p>
+      A backend engineer with experience in cloud infrastructure, CI/CD pipelines and container tools in the financial industry, with a focus on DevOps practices.
+    </p>
+
+    <p class="home-links">
+      <a href="https://github.com/benating/cv-static-site" target="_blank" rel="noopener">Site GitHub</a>
+      <a href="https://github.com/benating/cloud-resume-challenge" target="_blank" rel="noopener">Infrastructure GitHub</a>
+    </p>
+
+  </Layout>
+</template>
+
+<script>
+import Section from '~/components/Section.vue'
+
+export default {
+  metaInfo: {
+    title: 'Bernard\'s Profile'
+  },
+  components: {
+    Section
+  }
+}
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
